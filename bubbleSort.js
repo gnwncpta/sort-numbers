7f9361a6c76d4a46b57e419c6cd991285ef009d9
gnwncpta <gnwncpta@gmail.com>
2021-05-20 15:38:46 +0700

    Push

---
+++ bubbleSort.js
@@ -0,0 +1,31 @@
+// tpye "npm start" to run this code
+
+const swap = (e) => {
+    let x = e[1];
+    let y = e[2];
+    
+    return [x, y] = [y, x]
+}
+
+const asc = (arr) => {
+    let before = arr.join(' ')
+    console.log(`Before Sorted: ${before}`)
+
+    let compare = (arr.length - 1) * (arr.length - 1);
+
+    for(let i = 0; i < arr.length - 1; i++){
+
+        for(let j = 0; j < arr.length - 1; j++){
+
+            if(arr[j] > arr[j + 1]){
+                [ arr[j], arr[j + 1] ] = [ arr[j + 1], arr[j] ];
+            }
+
+        }
+        
+    }
+    let after = arr.join(' ')
+    console.log(`After Sorted: ${after}`)
+}
+
+asc([7, 3, 4, 9]);
